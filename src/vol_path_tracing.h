68d19f9a70cee527b8a2d01f16d0e12c54822930
Thomaswang0822 <thomaswang0822@outlook.com>
2024-02-10 17:33:27 -0800

    step 1

--- src/vol_path_tracing.h
+++ src/vol_path_tracing.h
@@ -6,8 +6,31 @@
 Spectrum vol_path_tracing_1(const Scene &scene,
                             int x, int y, /* pixel coordinates */
                             pcg32_state &rng) {
-    // Homework 2: implememt this!
-    return make_zero_spectrum();
+    // trace camera ray and intersect the scene
+    int w = scene.camera.width, h = scene.camera.height;
+    Vector2 screen_pos((x + next_pcg32_real<Real>(rng)) / w,
+                       (y + next_pcg32_real<Real>(rng)) / h);
+    Ray ray = sample_primary(scene.camera, screen_pos);
+    RayDifferential ray_diff = init_ray_differential(w, h);
+    std::optional<PathVertex> vertex_ = intersect(scene, ray, ray_diff);
+    if (!vertex_) {
+        return make_zero_spectrum();
+    }
+
+    PathVertex vertex = *vertex_;
+    // compute transmittance = exp(− σa * t)
+    Real t = distance(vertex.position, ray.org);  // seems like ray.tfar is not updated
+    // assert(vertex.exterior_medium_id == vertex.interior_medium_id);
+    Medium md = scene.media[scene.camera.medium_id];
+    Spectrum sigma_a = get_sigma_a(md, vertex.position);
+    Spectrum transmittance = exp(-t * sigma_a);
+
+    Spectrum Le = make_zero_spectrum();
+    if (is_light(scene.shapes[vertex.shape_id])) {
+        Le = emission(vertex, -ray.dir, scene);
+    }
+    // std::cout << transmittance << Le << std::endl;
+    return transmittance * Le;
 }
 
 // The second simplest volumetric renderer: 
